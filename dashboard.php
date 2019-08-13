9353be7fcc8f2350983a80fd7f741f10030b3188
Raylin Aquino <raylinaquino@gmail.com>
2019-08-13 18:45:07 -0400

    base

---
+++ dashboard.php
@@ -0,0 +1,40 @@
+<!-- Dashboard -->
+<section class="dashboard">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-6 col-sm-12">
+				<div class="features-slider">
+					<!-- Slider main container -->
+					<div class="swiper-container"> 
+						<div class="swiper-wrapper">
+							<!-- Slides -->
+							<div class="swiper-slide">
+								<aside>
+									<div class="wrap-img">
+										<i class="icon-instagram"></i>
+									</div>
+									<div class="wrap-info">
+										<h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+										<div class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium dolor maiores ipsam distinctio reiciendis ullam consequatur alias perferendis nihil dicta.</div>
+										<time datetime="2011-01-12">January 12th, 2011</time>
+									</div>
+								</aside>
+							</div> 
+
+						</div>
+						<!-- If we need pagination -->
+						<div class="swiper-pagination"></div>
+
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-12">
+				<aside class="dash-video">
+					<h2>Lorem ipsum dolor sit amet,<strong> consectetur adipisicing</strong> elit. Reiciendis, assumenda.</h2>
+					<iframe src="https://player.vimeo.com/video/259411563" width="100%" height="300" frameborder="0" title="Aqui un buen titulo" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>	
+				</aside>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- end Dashboard -->
---
+++ index.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<html lang="es" >
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Bootstrap CSS -->
+	<title>Written Sales</title>
+	<link href="images/favicon_edwards.ico" rel="shortcut icon" type="image/x-icon" />
+
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet"  type="text/css"  href="css/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/swiper.min.css"> 
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700,700i,900,900i&display=swap" rel="stylesheet">
+</head>
+<body>
+
+	<main>
+
+
+		<div class="sidebar-menu">
+			<div class="logo">
+				<img src="" width="100%" alt="">
+			</div>
+			<nav id="sidebar" class="sidebar-wrapper">
+				<ul>
+					<li><a href="#" class="trans-3">Users</a></li>
+					<li class="sidebar-dropdown">
+						<a href="#">
+							<i class="fa fa-tachometer-alt"></i>
+							<span>Sales Letter</span> &nbsp;
+							<span class="badge badge-pill badge-warning">New</span>
+						</a>
+						<div class="sidebar-submenu">
+							<ul>
+								<li>
+									<a href="#">
+										Dashboard 1
+									</a>
+								</li>
+								<li>
+									<a href="#">Dashboard 2</a>
+								</li>
+								<li>
+									<a href="#">Dashboard 3</a>
+								</li>
+							</ul>
+						</div>
+					</li>
+					<li><a href="#" class="trans-3">Users</a></li>
+					<li><a href="#" class="trans-3">Users</a></li>
+
+				</ul>
+			</nav>
+		</div> 
+		<!-- end Wrap Menu -->
+		<div class="section-content">
+			<header>
+				<div class="wrap-create-btn">
+					<div class="btn-group">
+						<button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Create Project
+						</button>
+						<div class="dropdown-menu">
+							<a class="dropdown-item" href="#">Action</a>
+							<a class="dropdown-item" href="#">Another action</a>
+							<a class="dropdown-item" href="#">Something else here</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Separated link</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="wrap-right">
+					<aside class="user-wrap">
+						<div class="top-profile">
+							<div class="img"></div>
+							<div class="right">
+								<div class="name">Andres Peguero</div>
+							<div class="job">Administrador</div>
+							<div class="actions">
+								
+							</div>
+							</div>
+						</div>
+					</aside>
+				</div>
+
+			</header>
+			<!-- Section -->
+			<section> 
+				<div class="section-top">
+					<nav aria-label="breadcrumb">
+						<ol class="breadcrumb">
+							<li class="breadcrumb-item"><a href="#">Home</a></li>
+							<li class="breadcrumb-item"><a href="#">Library</a></li>
+							<li class="breadcrumb-item active" aria-current="page">Data</li>
+						</ol>
+					</nav>
+				</div>
+
+				<?php include('dashboard.php') ?>
+
+			</section>
+			<!-- end Section -->
+		</div>
+		<footer>
+			<div class="copyright">
+				CopytRight 2019
+			</div>
+			<div class="footer-menu">
+				<a href="#" class="trans-3 underline">Uno dos</a>
+				<a href="#" class="trans-3 underline">Uno dos</a>
+				<a href="#" class="trans-3 underline">Uno dos</a>
+				<a href="#" class="trans-3 underline">Uno dos</a>
+			</div>
+		</footer>
+	</main>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/swiper.min.js"></script>  
+	<script src="js/jquery.marquee.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="js/main.js"></script> 
+
+</body>
+</html>
